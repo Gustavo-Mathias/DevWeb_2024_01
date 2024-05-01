9dc309b57a9f2e6f7760718e7150f25de4ba86d1
Gustavo-Mathias <gumathias1304@gmail.com>
2024-04-30 21:27:07 -0300

    update na atividade

--- JS/Aula_02/atividade/atividade.js
+++ JS/Aula_02/atividade/atividade.js
@@ -1,3 +1,3 @@
 var nome = 'Gustavo', altura = 1.78, peso = 62;
 
-console.log ('Meu nome é ' + nome + ' possuo uma altura de ' + altura + 'e um peso ' + peso +', meu IMC atual é de ' + peso / altura * altura);
+console.log ('Meu nome é ' + nome + ' possuo uma altura de ' + altura + 'e um peso ' + peso +', meu IMC atual é de ' + peso / (altura * altura));
