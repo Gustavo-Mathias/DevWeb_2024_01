f2e2c40a8984d7eaefffa04268a99ccee8c2ccaf
Gustavo-Mathias <gumathias1304@gmail.com>
2024-05-02 21:34:24 -0300

    realização de exercicios sobre strings

---
+++ JS/Aula_03/atividade/string.js
@@ -0,0 +1,26 @@
+let fruta01 = '   ';
+let fruta02 = 'Maçã';
+let fruta03 = 'Pera';
+let fruta04 = 'Manga';
+let fruta05 = 'Uva';
+
+console.log(fruta01.length, fruta02.length, fruta03.length, fruta04.length, fruta05.length);
+
+console.log(fruta01.substring(0, 3), fruta02.substring(0, 3), fruta03.substring(0, 3), fruta04.substring(0, 3), fruta05.substring(0, 3));
+
+console.log(fruta01.trim(),fruta02.trim(),fruta03.trim(),fruta04.trim(),fruta05.trim());
+
+console.log(fruta01, fruta02, fruta03, fruta04, fruta05);
+
+let = novaFruta01 = fruta01.replace ('',' carambola');
+let = novaFruta02 = fruta02.replace ('Maçã','banana');
+let = novaFruta03 = fruta03.replace('Pera','ameixa');
+let = novaFruta04 = fruta04.replace('Manga','tangerina');
+let = novaFruta05 = fruta05.replace('Uva','goiaba');
+
+console.log(novaFruta01,novaFruta02,novaFruta03,novaFruta04,novaFruta05);
+
+console.log(fruta01.toLowerCase(),fruta02.toLowerCase(),fruta03.toLowerCase(),fruta04.toLowerCase(),fruta05.toLowerCase());
+
+console.log('minha lista de fruta favoritas são'+  novaFruta01, novaFruta02, novaFruta03, novaFruta04, novaFruta05);
+
